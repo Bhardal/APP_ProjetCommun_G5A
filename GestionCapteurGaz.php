5e412fd7d0a39cc1ff35a1be2cd4911b355b8b4c
Rémy Maillard <remy85603@gmail.com>
2025-06-12 12:09:49 +0200

    MEDIUM: Changed from random values to use database

--- GestionCapteurGaz.php
+++ GestionCapteurGaz.php
@@ -4,10 +4,10 @@ require 'config.php';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-if (empty($_SESSION['user_id'])) {
-    header('Location: Connexion.php');
-    exit;
-}
+// if (empty($_SESSION['user_id'])) {
+//     header('Location: Connexion.php');
+//     exit;
+// }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -152,20 +152,49 @@ if (empty($_SESSION['user_id'])) {
 <script>
     let logs = [], actionManuel = false, chart;
 
+    function fetchDataFromDatabase() {
+        return fetch('./docs/getDataGaz.php')
+            .then(response => {
+                if (!response.ok) throw new Error('Network response was not ok');
+                return response.json();
+            })
+            .then(data => {
+                let last = data[data.length-1]
+                let id = last.id;
+                let value = last.intensite;
+                var dateString = last.date,
+                    dateTimeParts = dateString.split(" "),
+                    dateParts = dateTimeParts[0].split("-"),
+                    timeParts = dateTimeParts[1].split(":");
+                let date = new Date(dateParts[0], parseInt(dateParts[1], 10) - 1, dateParts[2], timeParts[0], timeParts[1], timeParts[2]);
+
+                const now = Math.floor(Date.now() / 1000);
+                return {
+                    ts: now,
+                    data: value
+                };
+            })
+
+    }
+
     function simulerLectureGaz(isManual) {
-        const val = Math.floor(Math.random()*500); // simulation ppm
-        document.getElementById('valeur').textContent = val + ' ppm';
-        const seuil = parseInt(document.getElementById('seuil').value,10);
-        if (!isManual && document.getElementById('auto').checked && !actionManuel) {
-            document.getElementById('etat-action').textContent =
-                '🛢️ Action : ' + (val < seuil ? 'ON (auto)' : 'OFF (auto)');
-        }
-        const now = new Date().toLocaleTimeString();
-        logs.unshift(`${now} – ${val} ppm`);
-        logs = logs.slice(0,5);
-        document.getElementById('historique').innerHTML =
-            logs.map(e=>`<li>${e}</li>`).join('');
-        updateGraph(now,val);
+        // const val = Math.floor(Math.random()*500); // simulation ppm
+        fetchDataFromDatabase()
+            .then(datas => {
+                const val = datas.data;
+                document.getElementById('valeur').textContent = val + ' ppm';
+                const seuil = parseInt(document.getElementById('seuil').value,10);
+                if (!isManual && document.getElementById('auto').checked && !actionManuel) {
+                    document.getElementById('etat-action').textContent =
+                        '🛢️ Action : ' + (val < seuil ? 'ON (auto)' : 'OFF (auto)');
+                }
+                const now = new Date().toLocaleTimeString();
+                logs.unshift(`${now} – ${val} ppm`);
+                logs = logs.slice(0,5);
+                document.getElementById('historique').innerHTML =
+                    logs.map(e=>`<li>${e}</li>`).join('');
+                updateGraph(now,val);
+            });
     }
 
     function toggleActionManuelle() {
---
+++ docs/getDataProx.php
@@ -0,0 +1,28 @@
+<?php
+require_once './dbConnexion.php';
+header('Content-Type: application/json');
+
+try {
+    $pdo = getConnection();
+    $stmt = $pdo->prepare("SELECT * FROM CapteurProximite ORDER BY ID DESC LIMIT 1");
+    $stmt->execute();
+
+    $allData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // for ($i = 0; $i < count($allData); $i++) {
+    //     echo "ID: " . $allData[$i]['id'] . ", Intensité: " . $allData[$i]['valeur_luminosite'] . ", Date: " . $allData[$i]['date_mesure'] . "\n";
+    // }
+    $data = [];
+    foreach ($allData as $row) {
+        $data[] = [
+            'id' => $row['ID'],
+            'intensite' => $row['Value'],
+            'date' => $row['Times']
+        ];
+    }
+    echo json_encode($data);
+} catch (PDOException $e) {
+    echo "Erreur lors de la récupération des categories : " . $e->getMessage();
+    exit();
+}
+?>
