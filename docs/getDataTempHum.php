949f6145364049dd13d94ea002267e32ea272bf9
Rémy Maillard <remy85603@gmail.com>
2025-06-13 12:40:17 +0200

    MEDIUM: ci: Added a way to get data for temperature and humidity

---
+++ docs/getDataTempHum.php
@@ -0,0 +1,29 @@
+<?php
+require_once './dbConnexion.php';
+header('Content-Type: application/json');
+
+try {
+    $pdo = getConnection();
+    $stmt = $pdo->prepare("SELECT * FROM capteur_temp_hum ORDER BY id DESC LIMIT 1");
+    $stmt->execute();
+
+    $allData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // for ($i = 0; $i < count($allData); $i++) {
+    //     echo "ID: " . $allData[$i]['id'] . ", Intensité: " . $allData[$i]['valeur_luminosite'] . ", Date: " . $allData[$i]['date_mesure'] . "\n";
+    // }
+    $data = [];
+    foreach ($allData as $row) {
+        $data[] = [
+            'id' => $row['id'],
+            'temp' => $row['temperature'],
+            'hum' => $row['humidite'],
+            'date' => $row['horodatage']
+        ];
+    }
+    echo json_encode($data);
+} catch (PDOException $e) {
+    echo "Erreur lors de la récupération des categories : " . $e->getMessage();
+    exit();
+}
+?>
